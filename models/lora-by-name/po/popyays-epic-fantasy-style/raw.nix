7c8c3cae497b095e16a67b57ceaabeaf84a08c77
ruben <ruben.cano96@gmail.com>
2024-06-11 01:07:25 +0200

    Add popyays

---
+++ models/lora-by-name/po/popyays-epic-fantasy-style/raw.nix
@@ -0,0 +1,19 @@
+{
+  id = 470073;
+  meta = { author = "popyay"; };
+  model = {
+    Pony = {
+      extension = "safetensors";
+      filename = "Fant5yP0ny";
+      id = 522995;
+      nsfwLevel = 7;
+    };
+    SDXL = {
+      extension = "safetensors";
+      filename = "EpicF4nta5yXL";
+      id = 560995;
+      nsfwLevel = 7;
+    };
+  };
+  type = "LORA";
+}
