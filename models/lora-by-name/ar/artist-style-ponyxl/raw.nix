fa4c39fcf215b2fd0e0d4545335adfca3fafcb6d
ruben <ruben.cano96@gmail.com>
2024-06-11 01:36:40 +0200

    artist-style-ponyxl

---
+++ models/lora-by-name/ar/artist-style-ponyxl/raw.nix
@@ -0,0 +1,56 @@
+{
+  id = 351303;
+  meta = { author = "nightdevv3"; };
+  model = {
+    "1.0" = {
+      extension = "safetensors";
+      filename = "rei_17_artist_style";
+      id = 392922;
+      nsfwLevel = 13;
+      trainedWords = "";
+    };
+    "v2.0" = {
+      extension = "safetensors";
+      filename = "artist_style";
+      id = 394694;
+      nsfwLevel = 1;
+      trainedWords = "";
+    };
+    "v3.0" = {
+      extension = "safetensors";
+      filename = "art";
+      id = 408807;
+      nsfwLevel = 1;
+      trainedWords = "";
+    };
+    "v4.0" = {
+      extension = "0";
+      filename = "art_v4";
+      id = 414038;
+      nsfwLevel = 13;
+      trainedWords = "";
+    };
+    "v5.0" = {
+      extension = "safetensors";
+      filename = "v5_1";
+      id = 494062;
+      nsfwLevel = 31;
+      trainedWords = "";
+    };
+    "v5.0 (Animagine)" = {
+      extension = "safetensors";
+      filename = "xl";
+      id = 514762;
+      nsfwLevel = 12;
+      trainedWords = "";
+    };
+    "v6.0" = {
+      extension = "safetensors";
+      filename = "v6";
+      id = 541926;
+      nsfwLevel = 7;
+      trainedWords = "";
+    };
+  };
+  type = "LORA";
+}
